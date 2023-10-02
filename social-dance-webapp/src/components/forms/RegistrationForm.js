ffeeeaa323b060bf2d94d87380e4d770163d63e3
vadim-yevtushenko <vadik.chicoto@gmail.com>
2023-10-02 22:00:35 +0300

    added error message min length password

--- social-dance-webapp/src/components/forms/RegistrationForm.js
+++ social-dance-webapp/src/components/forms/RegistrationForm.js
@@ -143,6 +143,7 @@ const RegistrationForm = () => {
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
                                     {errors?.password?.type === "required" && <p className="text-xs leading-5 text-red-700">Password is required.</p>}
+                                    {errors?.password?.type === "minLength" && <p className="text-xs leading-5 text-red-700">Min length must be 8 symbols.</p>}
                                 </div>
                             </div>
 
